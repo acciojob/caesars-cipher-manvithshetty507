f87ca48a289549b7005c843309c61c65f238888c
Manvith Kumar shetty <121666002+manvithshetty507@users.noreply.github.com>
2023-06-09 09:29:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -11,18 +11,26 @@ const lookup = {
 };
 
 function rot13(encodedStr){
-   let decodedArr = []; // Your Result goes here
-  // Only change code below this line
-	for(let i=0; i< encodedStr.length ;i++){
-		if(lookup[encodedStr[i] === undefined){
-			decodedArr.push(encodedStr[i]);
-		}
-		else{
-			decodedArr.push(lookup[encodedStr[i]]);
-		}
-	}
-  return decodedArr;
-}
+     let decodedArr = []; // Your Result goes here
+    // Only change code below this line
+      for(let i=0; i< encodedStr.length ;i++){
+        if(encodedStr[i] == ' '){
+            decodedArr.push(' ');
+            continue;
+        }
+        if(lookup[encodedStr[i] === undefined]){
+              decodedArr.push(encodedStr[i]);
+          }
+        else{
+              decodedArr.push(lookup[encodedStr[i]]);
+          }
+      }
+    let decodedString = "";
+    for(let c of decodedArr){
+        decodedString += c;
+    }
+    return decodedString;
+  }
 
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
 
